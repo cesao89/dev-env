cdaee069b543a86441e69920f8e8ec9a9eb02407
César Oliveira Domingos <cesar_web@live.com>
2019-05-10 11:51:30 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,10 @@
 project_path=`realpath "$1"`
 project=`basename $project_path`
 user=`whoami`
-docker run --rm --user $user --mount type=bind,source=${project_path},target=/home/$user/${project} -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -it dev-env:latest
-
+docker run --name dev-docker --privileged -p22 --user $user \
+    --mount type=bind,source=${project_path},target=/home/$user/${project} \
+    -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /home/$user/.kube:/home/$user/.kube \
+    -v /home/$user/.minikube:/home/$user/.minikube \
+    -it dev:1.0.0
